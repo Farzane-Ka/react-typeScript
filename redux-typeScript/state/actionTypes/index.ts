782c0f4f129f5e43d326474c8ad238656c028706
farzanekarami <50020784+Farzane-Ka@users.noreply.github.com>
2021-08-14 10:36:27 +0200

    Add files via upload

---
+++ redux-typeScript/state/actionTypes/index.ts
@@ -0,0 +1,5 @@
+export enum ActionTypes{
+    SEARCH_REPOSITORIES='repositories_search_action',
+    SEARCH_REPOSITORIES_SUCCESS='repositories_search_success_action',
+    SEARCH_REPOSITORIES_ERROR='repositories_search_error_action',
+}
